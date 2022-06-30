6cdbb31ecc30bbea311f1207095a86482390a3d2
Dmytro <vasylievdmytro04@gmail.com>
2022-06-30 03:28:49 -0700

    renewed api token

--- WebApi/Constant/Constants.cs
+++ WebApi/Constant/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public static string adress = "https://api.lyrics.ovh";
-        public static string apikey = "BQCH0JNoZjR5oDYYRGtWs7vPve6HoptTqaKG11aikHnxtXURj5AS3MAlyGWJlaHgv3oITPXz7lpxgBZH-OOy_Pk2tojaQKg-CY3xvY1MiYddfyMIG7PJclCMTXpzIFaGfucrBjHV3iYl40h-GHqYd5jt1PrEH3BK5Fil3CGtx0J5Twgv4-Aj3zegCj3zj5y2uks";
+        public static string apikey = "BQDS2pdykRU8NUqjJ5owxqeZvuawWWiQGzjToq1i7VU5XGhTlnRiegv0klPaXsJg1EZG8PurhzSaRIWKIQD4PHtXkSMvYldyzRhQPv8HzXU7h8vxK-GUp13dAuoSI3QKXyOtzsu79cyTyJ0mZcPRFTij7stv-X12iURSWG1BUHBlLk2JOi3jbh1Nzu8ni_PDF14";
         //public static string apikey = "BQBS9d-ZQ6DIsRe0vmOnsuW0LFqOcLu2OFeIPu_eH_20cS0_OeOvTzxWW-5NvQ3D0HKyRpaqKMjaDUsl5hx0ApBctwp9n2_rAlckrkFYEZnpBjaBPJjpnPiAV4LS8GrTPhHyuHEXL8yoJCWRqE0Unwyg_tiQTW0is61ut1Z1lEVkQP5Co4RMLYED9cf1-FN4wZi5";
     }
 }
